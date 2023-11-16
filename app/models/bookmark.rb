b38b0aa859fac7a7bff7c4959506eef6193402ca
Victor Bostaetter <victor.bostaetter@docent-art.com>
2023-11-16 11:26:19 +0100

    finishing models and specs

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,10 @@
 class Bookmark < ApplicationRecord
-  belongs_to :movies
-  belongs_to :lists
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, length: { minimum: 6 }, presence: true
+  validates :movie_id, uniqueness: { scope: :list_id }
+  # validates :list_id, uniqueness: { scope: :movie_id }
+  # validates :movie_id, presence: true
+  # validates :list_id, presence: true
 end
